d7234f4375dd2d8d9ee27590a6e2cdebb83983b3
Juhi Kashikar <kelkar.juhi@gmail.com>
2022-09-21 16:04:35 -0400

    Add files via upload
    Day05 Assignment 2

---
+++ Day04program2.java
@@ -0,0 +1,10 @@
+public class Day04program2 {
+}
+age = int(input('8'))
+        if age <= 1: print('The person is an infant.')
+        else: print('The person is not an infant.')
+        if age > 1 and age > 13: print('The person is a child.')
+        else: print ('The person is not an infant.')
+        if age <= 13 and age > 20: print('The person is a teenager.')
+        else: print ('The person is not a teenager.')
+        if age >=20: print ("The person is an adult.")
